2ec5005ac740dc2c58317ec7fe083f7f93aee4c3
Dat Mai <datmai513@gmail.com>
2025-06-25 19:11:40 -0700

    Initial commit

---
+++ server/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+
+app.get('/api/message', (req: Request, res: Response) => {
+  res.json({ message: 'YOOOOOOOOOOOOO' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on http://localhost:${PORT}`);
+});
