2b9f7d768da1e0911746af080b619ff30e96066c
dylan <dodylan009@yahoo.com>
2025-07-08 19:02:58 -0700

    basic css layout

--- client/src/components/layout/navbar/navbar.tsx
+++ client/src/components/layout/navbar/navbar.tsx
@@ -4,11 +4,7 @@ import classes from "../layout.module.css"
 const Navbar = () => {
   return (
   <nav className={classes.navbar}>
-    <ul>
-      <li><Link to="/competitions">Competitions</Link></li>
-      <li><Link to="/contestants">Contestants</Link></li>
-      <li><Link to="/leaderboard">Leaderboard</Link></li>
-    </ul>
+ 
   </nav>
   )
 };
--- client/src/pages/contestants.tsx
+++ client/src/pages/contestants.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import classes from "./competitions.module.css"
 
 const Contestants: React.FC = () => {
   return (
-    <div>
+    <div className={classes.title}>
       <h1>WELCOME TO THE CONTESTANT</h1>
     </div>
   );
