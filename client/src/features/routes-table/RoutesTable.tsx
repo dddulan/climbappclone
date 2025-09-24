98155afc3e17a06e69f9ee5d8185d4df0092d1c8
dylan <dodylan009@yahoo.com>
2025-09-23 23:24:46 -0700

    added date

--- client/src/features/routes-table/RoutesTable.tsx
+++ client/src/features/routes-table/RoutesTable.tsx
@@ -4,9 +4,10 @@ import React, { useContext, useEffect, useState } from "react";
 import { DataRow, DataTable } from "@/components/ui/table";
 import { Plus } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import { getRoutesById } from "@/services/routeService";
+import { getRoutesById, saveRoutes } from "@/services/routeService";
 import { CompContext } from "@/components/layout/layout";
 import { Input } from "@/components/ui/input";
+import { Calendar } from "@/components/ui/calendar";
 import {
   Select,
   SelectContent,
@@ -15,7 +16,9 @@ import {
   SelectLabel,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select"
+} from "@/components/ui/select";
+import { se } from "date-fns/locale";
+import { setDate } from "date-fns";
 interface tableProps {
   isEdit: boolean;
   toggleEditing: (isSelected: boolean) => void;
@@ -25,9 +28,13 @@ export const RoutesTable: React.FC<tableProps> = ({ toggleEditing }) => {
   const [routes, setRoutes] = useState<Route[]>([]); // original copy of routes, update when user saves any edits
   const [rows, setRows] = useState<Route[]>([]); // rows for data table
   const [currentRoute, setCurrentRoute] = useState<Route>(); // orginal copy of currently selected route
+  const [selectedGrade, setSelectedGrade] = useState<string>("");
+  const [selectedRouteColor, setSelectedRouteColor] = useState<string>("");
+  const [scoreValue, setScoreValue] = useState<string>("");
+const [date, setDate] = React.useState<Date | "">(new Date())
   const ctx = useContext(CompContext);
 
-  var temp = routeColumns.slice().splice(1);
+  const temp = routeColumns.slice().splice(1);
 
   useEffect(() => {
     loadData();
@@ -59,14 +66,24 @@ export const RoutesTable: React.FC<tableProps> = ({ toggleEditing }) => {
       id: 0,
       name: "",
       number: 0,
-      grade: "",
-      color: "",
-      competition_id: 0,
+      grade: selectedGrade,
+      color: selectedRouteColor,
+      competition_id: ctx?.comp.id || 0,
       point_value: 0,
-      set_date: "",
+      set_date:  date ? date.toISOString().split('T')[0] : "",
     };
 
     setRows([...rows, blankRow]);
+    if (ctx?.comp.id) {
+      blankRow.competition_id = ctx.comp.id;
+    }
+    console.log("ADD ROW", blankRow);
+    /* 
+   saveRoutes([...rows, blankRow]).then((res) => {
+      loadData();
+      
+    });
+   */
   };
 
   return (
@@ -75,57 +92,57 @@ export const RoutesTable: React.FC<tableProps> = ({ toggleEditing }) => {
 
       <div className="px-5 border-1 rounded-sm bg-white shadow-xl ">
         <div className="flex space-x-2 pt-5">
- 
-
           {/* ADD ROW COMPOENENT GOES HERE */}
- <Select>
-      <SelectTrigger className="w-[90px]">
-        <SelectValue placeholder="Grade" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectLabel>Grade</SelectLabel>
-          <SelectItem value="v1">v1</SelectItem>
-          <SelectItem value="v2">v2</SelectItem>
-          <SelectItem value="v3">v3</SelectItem>
-          <SelectItem value="v4">v4</SelectItem>
-          <SelectItem value="v5">v5</SelectItem>
-          <SelectItem value="v6">v6</SelectItem>
-          <SelectItem value="v7">v7</SelectItem>
-          <SelectItem value="v8">v8</SelectItem>
-          <SelectItem value="v9">v9</SelectItem>
-          <SelectItem value="v10">v10+</SelectItem>
+          <Select onValueChange={(value) => setSelectedGrade(value)}>
+            <SelectTrigger className="w-[90px]">
+              <SelectValue placeholder="Grade" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Grade</SelectLabel>
+                <SelectItem value="v1">v1</SelectItem>
+                <SelectItem value="v2">v2</SelectItem>
+                <SelectItem value="v3">v3</SelectItem>
+                <SelectItem value="v4">v4</SelectItem>
+                <SelectItem value="v5">v5</SelectItem>
+                <SelectItem value="v6">v6</SelectItem>
+                <SelectItem value="v7">v7</SelectItem>
+                <SelectItem value="v8">v8</SelectItem>
+                <SelectItem value="v9">v9</SelectItem>
+                <SelectItem value="v10">v10+</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
 
+          <Select onValueChange={(value) => setSelectedRouteColor(value)}>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Color" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Fruits</SelectLabel>
+                <SelectItem value="red">Red</SelectItem>
+                <SelectItem value="blue">Blue</SelectItem>
+                <SelectItem value="green">Green</SelectItem>
+                <SelectItem value="yellow">Yellow</SelectItem>
+                <SelectItem value="pink">Pink</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+          {/* Score*/}
+          <Input placeholder="ex.150" type="number" value={scoreValue} onChange={(e)=>setScoreValue(e.target.value)}className="w-40" />
+          {/* Date*/}
+          <Calendar 
+            mode="single"
+            selected={date}
+            onSelect={setDate}
+            className="rounded-lg border"
 
-
-        </SelectGroup>
-      </SelectContent>
-    </Select>
-
- <Select>
-      <SelectTrigger className="w-[180px]">
-        <SelectValue placeholder="Color" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectLabel>Fruits</SelectLabel>
-          <SelectItem value="red">Red</SelectItem>
-          <SelectItem value="blue">Blue</SelectItem>
-          <SelectItem value="green">Green</SelectItem>
-          <SelectItem value="yellow">Yellow</SelectItem>
-          <SelectItem value="pink">Pink</SelectItem>
-        </SelectGroup>
-      </SelectContent>
-    </Select>
-
-
-          <Input placeholder="ex.150" className="w-40" />
-          <Input placeholder="Date" className="w-40" />
-
-         <Button size="sm" onClick={addRow}>
+          />
+          {/* Add Button */}
+          <Button size="sm" onClick={addRow}>
             <Plus />
           </Button>
-
         </div>
 
         <div className="mx-auto pt-5 w-175">
